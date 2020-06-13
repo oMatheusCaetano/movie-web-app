b8ee34d1cb452a0b1b9f6d524f2b660914efd4a0
Matheus Caetano <caetanomatheus876@gmail.com>
2020-06-13 19:35:27 -0300

    Insertindo reação no Cards de filmes ao Hover do mouse

--- src/components/MaMediaCard.vue
+++ src/components/MaMediaCard.vue
@@ -4,11 +4,11 @@
       <div class="p-1 rounded-circle bg-light">{{ media.vote_average | formatRate }}</div>
     </div>
     <router-link
-      class="text-decoration-none font-weight-bold text-dark"
+      class=" text-decoration-none font-weight-bold text-dark"
       to="#"
     >
       <img
-        class="mb-3 rounded shadow-sm"
+        class="mediaImage mb-3 rounded shadow-sm"
         :src="`https://image.tmdb.org/t/p/w200/${this.media.poster_path}`"
         alt="picture" />
       <span>{{ media.title }}</span>
@@ -28,9 +28,18 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .mediaRating {
-  bottom: 140px;
+  z-index: 1;
   left: 5px;
+  bottom: 140px;
+}
+
+.mediaImage {
+  transition: .2s;
+
+  &:hover {
+    filter: brightness(50%);
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from './router'
 import store from './store'
 import './bootstrap'
 
-require('dotenv').config()
-
 Vue.config.productionTip = false
 
 new Vue({
